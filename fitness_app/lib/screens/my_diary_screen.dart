5fac4f2baf5cefedaccca43b228812c34dcb76cc
RGB <ragabelsayed37@gmail.com>
2021-01-04 21:27:23 +0200

    Adding customappBar widget to MyDiary

--- fitness_app/lib/screens/my_diary_screen.dart
+++ fitness_app/lib/screens/my_diary_screen.dart
@@ -7,6 +7,8 @@ import 'package:fitness_app/widgets/mediterranean_diet_view.dart';
 import 'package:fitness_app/widgets/title_view.dart';
 import 'package:fitness_app/widgets/water_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyDiaryScreen extends StatefulWidget {
   static const routeName = '/';
@@ -17,8 +19,12 @@ class MyDiaryScreen extends StatefulWidget {
 
 class _MyDiaryScreenState extends State<MyDiaryScreen>
     with SingleTickerProviderStateMixin {
-  List<Widget> _listViews = [];
   AnimationController _animationController;
+  List<Widget> _listViews = [];
+  Animation _topBarAnimation;
+  double _topBarOpacity = 0.0;
+  final ScrollController _scrollController = ScrollController();
+  DateTime _selectedDate;
 
   @override
   void initState() {
@@ -27,7 +33,35 @@ class _MyDiaryScreenState extends State<MyDiaryScreen>
       vsync: this,
       duration: const Duration(milliseconds: 600),
     );
+    _topBarAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Interval(0, 0.5, curve: Curves.fastOutSlowIn),
+    ));
     _addAllListData();
+
+    _scrollController.addListener(() {
+      if (_scrollController.offset >= 24) {
+        if (_topBarOpacity != 1.0) {
+          setState(() {
+            _topBarOpacity = 1.0;
+          });
+        }
+      } else if (_scrollController.offset <= 24 &&
+          _scrollController.offset >= 0) {
+        if (_topBarOpacity != _scrollController.offset / 24) {
+          setState(() {
+            _topBarOpacity = _scrollController.offset / 24;
+          });
+        }
+      } else if (_scrollController.offset <= 0) {
+        if (_topBarOpacity != 0.0) {
+          setState(() {
+            _topBarOpacity = 0.0;
+          });
+        }
+      }
+    });
   }
 
   @override
@@ -189,6 +223,22 @@ class _MyDiaryScreenState extends State<MyDiaryScreen>
     );
   }
 
+  void _dataPicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2021),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
   Future<bool> _getData() async {
     await Future<dynamic>.delayed(const Duration(milliseconds: 50));
     return true;
@@ -203,6 +253,7 @@ class _MyDiaryScreenState extends State<MyDiaryScreen>
         body: Stack(
           children: [
             _getMainListViewUI(),
+            _getAppBarUI(),
           ],
         ),
       ),
@@ -218,6 +269,7 @@ class _MyDiaryScreenState extends State<MyDiaryScreen>
           return const SizedBox();
         } else {
           return ListView.builder(
+            controller: _scrollController,
             padding: EdgeInsets.only(
               top: AppBar().preferredSize.height + _mediaQuery.padding.top + 24,
               bottom: _mediaQuery.padding.bottom + 62,
@@ -231,6 +283,133 @@ class _MyDiaryScreenState extends State<MyDiaryScreen>
           );
         }
       },
+    );
+  }
+
+  Widget _getAppBarUI() {
+    return Column(
+      children: [
+        AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            return FadeTransition(
+              opacity: _topBarAnimation,
+              child: Transform(
+                transform: Matrix4.translationValues(
+                    0.0, 30 * (1.0 - _topBarAnimation.value), 0.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: AppTheme.white.withOpacity(_topBarOpacity),
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(32),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppTheme.gray.withOpacity(0.4 * _topBarOpacity),
+                        offset: const Offset(1.1, 1.1),
+                        blurRadius: 10,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).padding.top,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: 16,
+                          right: 16,
+                          top: 16 - 8.0 * _topBarOpacity,
+                          bottom: 12 - 0.8 * _topBarOpacity,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  'My Diary',
+                                  textAlign: TextAlign.left,
+                                  style: GoogleFonts.roboto(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 22 + 6 - 6 * _topBarOpacity,
+                                    letterSpacing: 1.2,
+                                    color: AppTheme.darkerText,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 38,
+                              width: 38,
+                              child: InkWell(
+                                highlightColor: Colors.transparent,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(32),
+                                ),
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.keyboard_arrow_left,
+                                  color: AppTheme.gray,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 8),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 8),
+                                    child: IconButton(
+                                      icon: Icon(Icons.calendar_today),
+                                      color: AppTheme.gray,
+                                      iconSize: 18,
+                                      onPressed: _dataPicker,
+                                    ),
+                                  ),
+                                  Text(
+                                    _selectedDate == null
+                                        ? '15 May'
+                                        : '${DateFormat.MMMd().format(_selectedDate)}',
+                                    textAlign: TextAlign.left,
+                                    style: GoogleFonts.roboto(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 18,
+                                      letterSpacing: -0.2,
+                                      color: AppTheme.darkerText,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: 38,
+                              width: 38,
+                              child: InkWell(
+                                highlightColor: Colors.transparent,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(32),
+                                ),
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.keyboard_arrow_right,
+                                  color: AppTheme.gray,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
